f628e9302ff9173cb25f25d9f973861b051758ab
kakkoiirus <bagadin@inbox.ru>
2017-03-22 22:53:35 +0500

    JS для модального окна

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
+var toCart = document.querySelector(".to-cart");
+var btnOrder = document.querySelector(".btn--order");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
+
+btnOrder.addEventListener("click", function(event) {
+  event.preventDefault();
+  toCart.classList.add("to-cart--opened")
+});
+
+window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (toCart.classList.contains("to-cart--opened")) {
+            toCart.classList.remove("to-cart--opened");
+          }
+        }
+      });
