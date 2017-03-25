0a237f7945c606fb458aab0e94d3c275d47b43e9
kakkoiirus <bagadin@inbox.ru>
2017-03-25 23:36:25 +0500

    Карта с кастомным маркером

---
+++ js/map-script.js
@@ -0,0 +1,16 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map("map", {
+            center: [59.93865798144006,30.322893567459108],
+            zoom: 17
+        }, {
+            searchControlProvider: "yandex#search"
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+            iconLayout: "default#image",
+            iconImageHref: "../img/icon-map-pin.svg",
+            iconImageSize: [67, 100],
+            iconImageOffset: [-30, -100]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
