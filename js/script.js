a480093af0c54ec45c2273dd4432c87d4e70aa8e
kakkoiirus <bagadin@inbox.ru>
2017-03-25 21:54:05 +0500

    Правка сетки, добавление графики

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var navToggle = document.querySelector(".main-nav__toggle");
 
 var toCart = document.querySelector(".to-cart");
 var btnOrder = document.querySelector(".btn--order");
+var overlay = document.querySelector(".overlay");
 
 navMain.classList.remove("main-nav--nojs");
 
@@ -18,13 +19,15 @@ navToggle.addEventListener("click", function() {
 
 btnOrder.addEventListener("click", function(event) {
   event.preventDefault();
-  toCart.classList.add("to-cart--opened")
+  toCart.classList.add("to-cart--opened");
+  overlay.classList.add("overlay--opened");
 });
 
 window.addEventListener("keydown", function(event) {
         if (event.keyCode === 27) {
           if (toCart.classList.contains("to-cart--opened")) {
             toCart.classList.remove("to-cart--opened");
+            overlay.classList.remove("overlay--opened");
           }
         }
       });
